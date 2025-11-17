9b15d8bb1e371677b5463ded7aa58c8ea20c0cfb
shdwu <shdwu91@gmail.com>
2025-11-17 17:35:53 +0800

    👍

--- configs/nvim/lua/plugins/dap.lua
+++ configs/nvim/lua/plugins/dap.lua
@@ -1,7 +1,62 @@
 return {
-  "jay-babu/mason-nvim-dap.nvim",
-  dependences = {
-    "williamboman/mason.nvim",
-    "mfussenegger/nvim-dap"
-  }
+  "mfussenegger/nvim-dap",
+  dependencies = {
+    "nvim-neotest/nvim-nio",
+    "rcarriga/nvim-dap-ui",      -- 可视化 UI 面板
+    "jay-babu/mason-nvim-dap.nvim",
+    "williamboman/mason.nvim",   -- 自动安装 DAP 适配器
+    "theHamsta/nvim-dap-virtual-text",
+  },
+  config = function()
+    require("mason").setup()
+    require("mason-nvim-dap").setup({
+      ensure_installed = { "php" }, -- 自动安装 php-debug-adapter
+      automatic_setup = true,
+      handlers = {},
+    })
+    require("nvim-dap-virtual-text").setup()
+
+    local dap = require("dap")
+    local dapui = require("dapui")
+    dapui.setup()
+
+    vim.keymap.set('n', '<F5>', function() dap.continue() end)
+    vim.keymap.set("n", "<F7>", dapui.toggle, { desc = "Debug: See last session result." })
+    vim.keymap.set("n", "<F9>", function() dap.toggle_breakpoint() end, { desc = "Debug: See last session result." })
+    vim.keymap.set('n', '<F10>', function() dap.step_over() end)
+    vim.keymap.set('n', '<F11>', function() dap.step_into() end)
+    vim.keymap.set('n', '<F12>', function() dap.step_out() end)
+
+		dap.listeners.before.attach.dapui_config = function()
+			dapui.open()
+		end
+		dap.listeners.before.launch.dapui_config = function()
+			dapui.open()
+		end
+		dap.listeners.before.event_terminated.dapui_config = function()
+			dapui.close()
+		end
+		dap.listeners.before.event_exited.dapui_config = function()
+			dapui.close()
+		end
+
+		dap.configurations.php = {
+			{
+				name = "PHP: Listen for Xdebug",
+				port = 9003,
+				request = "launch",
+				type = "php",
+				breakpoints = {
+					exception = {
+						Notice = false,
+						Warning = false,
+						Error = false,
+						Exception = false,
+						["*"] = false,
+					},
+				},
+			},
+		}
+  end
 }
+
--- configs/nvim/lua/plugins/lsp.lua
+++ configs/nvim/lua/plugins/lsp.lua
@@ -1,21 +1,30 @@
 return {
-    "mason-org/mason-lspconfig.nvim",
-    opts = {
+  "williamboman/mason-lspconfig.nvim",
+  dependencies = {
+    { "mason-org/mason.nvim", opts = {} },
+    "neovim/nvim-lspconfig",
+  },
+  config = function()
+    local util = require("lspconfig.util")
+
+    require("mason-lspconfig").setup({
+      ensure_installed = { "lua_ls" }, -- 不再写 volar
       handlers = {
         ["hls"] = function()
-          require("lspconfig").hls.setup()
-        end
-      }
-    },
-    ft = { "lua", "javascript", "typescript", "python", "go", "haskell", "php", "vue"},
-    dependencies = {
-        { "mason-org/mason.nvim", opts = {} },
-        "neovim/nvim-lspconfig",
-    },
-    keys = {
-      {"<leader>gd", "<cmd>lua vim.lsp.buf.definition()<cr>", desc = "Lsp Goto Definition"},
-      {"<leader>gi", "<cmd>lua vim.lsp.buf.implementation()<cr>", desc = "Lsp Goto Implementation"},
-      {"<leader>gr", "<cmd>lua vim.lsp.buf.refenerces()<cr>", desc = "Lsp Goto References"},
-      {"<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<cr>", desc = "Show Can Use Code Action"},
-    }
+          vim.lsp.setup('hls')
+        end,
+
+        function(server_name)
+          vim.lsp.setup(server_name)
+        end,
+      },
+    })
+  end,
+  ft = { "lua", "javascript", "typescript", "python", "go", "haskell", "php", "vue" },
+  keys = {
+    { "<leader>gd", "<cmd>lua vim.lsp.buf.definition()<cr>",     desc = "Lsp Goto Definition" },
+    { "<leader>gi", "<cmd>lua vim.lsp.buf.implementation()<cr>", desc = "Lsp Goto Implementation" },
+    { "<leader>gr", "<cmd>lua vim.lsp.buf.references()<cr>",     desc = "Lsp Goto References" },
+    { "<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<cr>",    desc = "Show Can Use Code Action" },
+  },
 }
